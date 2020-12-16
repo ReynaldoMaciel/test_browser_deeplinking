c4076f8228a17b7b283299eddcb2933c02da90bd
Reynaldo Marinho Monteiro Maciel <reynaldommaciel@gmail.com>
2020-12-16 09:52:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const fallbackToPlayStore = function() {
-  window.location.replace('https://play.google.com/store?hl=pt_BR');
+  window.location.replace('https://play.google.com/store/apps/details?id=br.com.coocafe.coocafecop');
 };
 
 const fallbackToAppleStore = function() {
-  window.location.replace('https://www.apple.com/br/ios/app-store/');
+  window.location.replace('https://apps.apple.com/br/app/coocafe/id1457360330');
 };
 
 const openApp = function() {
-  
-  if (navigator.appVersion.includes('Android')) {
-    setTimeout(fallbackToPlayStore, 250);
-  }
-  
   if (navigator.appVersion.includes('iPhone') || navigator.appVersion.includes('iPad')) {
     setTimeout(fallbackToAppleStore, 250);
+  } else {
+    setTimeout(fallbackToPlayStore, 250);
   }
 };
 
